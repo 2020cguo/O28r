f57828fd2e1263a1a25408502997aaad80513628
clg8rd <clg8rd@virginia.edu>
2023-12-01 14:48:56 -0500

    pmd parser multiple files

--- oxygenator.sh
+++ oxygenator.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 # call pmd
 FILE_DIR="test_runs/"
-./pmd-bin-7.0.0-SNAPSHOT/bin/pmd check -d $FILE_DIR --rulesets rulesets/java/quickstart.xml > test_output.txt
-python3 -c "import pmd_parser; pmd_parser.parse('test_output.txt', 'parser_output.txt')"
+SOURCE_FILE="Zookeeper.java"
+OUTPUT_DIR="output_files"
+./pmd-bin-7.0.0-SNAPSHOT/bin/pmd check -d $FILE_DIR --rulesets rulesets/java/quickstart.xml > $OUTPUT_DIR/pmd_output.txt
+python3 -c "import pmd_parser; pmd_parser.parse('${OUTPUT_DIR}/pmd_output.txt', '${OUTPUT_DIR}/parser_output')"
 # call candace's schtuff
 cd ../javaparser-maven
 export PATH=$PATH:apache-maven-3.9.5/bin
 mvn clean install
-java -jar target/javaparser-maven-sample-1.0-SNAPSHOT-shaded.jar "../O28r/${FILE_DIR}/Test.java" "../O28r/parser_output.txt" > ../O28r/error_methods.txt
+java -jar target/javaparser-maven-sample-1.0-SNAPSHOT-shaded.jar "../O28r/${FILE_DIR}/${SOURCE_FILE}" "../O28r/${OUTPUT_DIR}/parser_output.txt" > ../O28r/${OUTPUT_DIR}/error_methods.txt
 # call java callgraph
 cd ../O28r/java-callgraph
 export PATH=$PATH:../apache-maven-3.9.5/bin
 mvn install -DskipTests
-jar -cvf Test.jar ../$FILE_DIR/*
-java -jar target/javacg-0.1-SNAPSHOT-static.jar Test.jar > "../callgraph.txt"
+jar -cvf CallGraph.jar ../$FILE_DIR/*
+java -jar target/javacg-0.1-SNAPSHOT-static.jar CallGraph.jar > "../${OUTPUT_DIR}/callgraph.txt"
 cd ..
-python3 -c "import graph_constructor; graph_constructor.main('callgraph.txt', 'error_methods.txt')"
+python3 -c "import graph_constructor; graph_constructor.main('${OUTPUT_DIR}/callgraph.txt', '${OUTPUT_DIR}/error_methods.txt')"
--- pmd_parser.py
+++ pmd_parser.py
@@ -1,14 +1,28 @@
+import os
+
+# dir_name = "parser_output"
+
 #TODO: account for no empty try catches
-def parse(input_file, output_file):
-    output_lines = []
+def parse(input_file, output_dir):
+    file_map = {}
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
     with open(input_file) as f:
         data=f.readlines()
     for line in data:
         line_arr = line.split(":")
+        if line_arr[0] not in file_map.keys():
+            file_map[line_arr[0]] = []
         if line_arr[2] == "\tEmptyCatchBlock":
-            output_lines.append(line_arr[1])
+            file_map[line_arr[0]].append(line_arr[1])
     
-    with open(output_file, 'w') as f:
-        for line in output_lines:
-            f.write(line+"\n")
+    for k in file_map.keys():
+        file_name = k.split('.')[0]     # gets rid of the .java
+        file_name = file_name.replace("/", "%")
+        with open(output_dir + "/" + file_name + ".txt", 'w') as f:
+            for line in file_map[k]:
+                f.write(line+"\n")
+
+
 
