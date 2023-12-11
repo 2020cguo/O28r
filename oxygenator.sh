e27e81f02852742ef77ab40f426b0d1c89ae7854
clg8rd <clg8rd@virginia.edu>
2023-12-11 15:08:31 -0500

    move javaparser into repo

--- oxygenator.sh
+++ oxygenator.sh
@@ -2,24 +2,24 @@
 
 ### PATHS ###
 # parent directory of all source code, relative to O28r
-SOURCE_DIR="zookeeper/"
+SOURCE_DIR="flume/"
 # parent directory of output files, relative to O28r
-OUTPUT_DIR="zookeeper_output"
+OUTPUT_DIR="flume_output"
 # parent directory containing jar files, relative to java-callgraph
-JAR_FILE_DIR="zookeeper_lib/"
+JAR_FILE_DIR="flume_lib/"
 
 ### CALL AND PARSE PMD ###
 ./pmd-bin-7.0.0-SNAPSHOT/bin/pmd check -d $SOURCE_DIR --rulesets rulesets/java/quickstart.xml > $OUTPUT_DIR/pmd_output.txt
 python3 -c "import pmd_parser; pmd_parser.parse('${OUTPUT_DIR}/pmd_output.txt', '${OUTPUT_DIR}/parser_output')"
 
 ### CALL JAVAPARSER ###
-cd ../javaparser-maven
+cd javaparser-maven
 export PATH=$PATH:apache-maven-3.9.5/bin
 mvn clean install
-java -jar target/javaparser-maven-sample-1.0-SNAPSHOT-shaded.jar "../O28r/${OUTPUT_DIR}/parser_output" "../O28r/${SOURCE_DIR}" > ../O28r/${OUTPUT_DIR}/error_methods.txt
+java -jar target/javaparser-maven-sample-1.0-SNAPSHOT-shaded.jar "../${OUTPUT_DIR}/parser_output" "../${SOURCE_DIR}" > ../${OUTPUT_DIR}/error_methods.txt
 
 ### CALL JAVA CALLGRAPH ###
-cd ../O28r/java-callgraph
+cd ../java-callgraph
 export PATH=$PATH:../apache-maven-3.9.5/bin
 mvn install -DskipTests
 rm -f "../${OUTPUT_DIR}/callgraph.txt"
